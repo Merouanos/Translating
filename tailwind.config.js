858a44b6aa3abe0c48583cd91812d8272e49867e
Merouanos <jessemerouane@gmail.com>
2026-03-05 13:27:29 +0100

    Clean build for Cornelia portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: [
+    "./*.html",            // Scans all HTML files in your root folder
+    "./src/**/*.{js,ts}",  // Scans all JS/TS files in a src folder (if you have one)
+    "./index.html"         // Explicitly scans your main file
+  ],
   theme: {
     extend: {},
   },
